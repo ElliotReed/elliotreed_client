59482b54006faa8657a07e7cba9dcc61602bbd20
elliotreed <elliot@elliotreed.net>
2025-04-25 12:26:49 -0600

    Switched icons to Iconify

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -3,10 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import path from 'node:path';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), react()],
+  integrations: [mdx(), react(), icon()],
   vite: {
     resolve: {
       alias: {
