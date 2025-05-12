a3399b2bccb43818171db461de51401ac873fe30
elliotreed <elliot@elliotreed.net>
2025-05-12 08:48:42 -0600

    removed gatsby

--- src/components/ILoveParisGallery/ILoveParisGallery.tsx
+++ src/components/ILoveParisGallery/ILoveParisGallery.tsx
@@ -1,34 +1,37 @@
-import * as React from "react";
-
-import { StaticImage } from "gatsby-plugin-image";
+import { Image } from "astro:assets"
 
 import ImageGallery from "../ImageGallery";
 
+import rootsImage from "./iloveparis-roots.jpg";
+import kuvoImage from "./iloveparis-kuvo.jpg";
+import bohemianImage from "./iloveparis-bohemian.jpg";
+import redImage from "./iloveparis-red.jpg";
+
 interface ILoveParisGalleryProps {
-  onClick: React.ReactEventHandler
+    onClick: React.ReactEventHandler
 }
 
 export default function ILoveParisGallery({ onClick }: Readonly<ILoveParisGalleryProps>) {
-  return (
-    <div onClick={onClick}>
-      <ImageGallery>
-        <StaticImage
-          src="./iloveparis-roots.jpg"
-          alt="Kristi Stice performing onstage with Gypsy Swing Revue for Blue Roots Denver."
-        />
-        <StaticImage
-          src="./iloveparis-kuvo.jpg"
-          alt=""
-        />
-        <StaticImage
-          src="./iloveparis-bohemian.jpg"
-          alt=""
-        />
-        <StaticImage
-          src="./iloveparis-red.jpg"
-          alt=""
-        />
-      </ImageGallery>
-    </div>
-  );
+    return (
+        <div onClick={onClick}>
+            <ImageGallery>
+                <Image
+                    src={rootsImage}
+                    alt="Kristi Stice performing onstage with Gypsy Swing Revue for Blue Roots Denver."
+                />
+                <Image
+                    src={kuvoImage}
+                    alt=""
+                />
+                <Image
+                    src={bohemianImage}
+                    alt=""
+                />
+                <Image
+                    src={redImage}
+                    alt=""
+                />
+            </ImageGallery>
+        </div>
+    );
 }
